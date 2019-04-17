1b39309b686d73702090717d3a3a4d95bc312b92
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-17 17:03:57 +0200

    refactored city collecting

--- src/api/OpenAq.js
+++ src/api/OpenAq.js
@@ -1,11 +1,44 @@
+import { stringifyRequest } from "./Common";
+
 const baseUrl = "https://api.openaq.org/v1";
 const latestEndpoint = "/latest";
 
-const esc = encodeURIComponent;
-const stringifyRequest = (url, params) =>
-  `${url}?${Object.keys(params)
-    .map(k => esc(k) + "=" + esc(params[k]))
-    .join("&")}`;
+//formats result array to simpler form
+//discarding irrevelant data
+const formatResults = results =>
+  results.map(res => ({
+    country: res.country,
+    city: res.city,
+    location: res.location,
+    measurement: res.measurements[0].value,
+    unit: res.measurements[0].unit
+  }));
+
+//sorts result array by measurement in descending order
+//(worse air quality first)
+const sortResults = results =>
+  results.sort((resA, resB) => resB.measurement - resA.measurement);
+
+//iterates the array and checks if current city has previous occurence with different measurement
+//skips duplicates, leaving occurence with worse params
+//assumes data is sorted by air quality
+//also, iteration ends if collected enough cities (reached limit)
+const limitAndFilterCityDuplicates = (results, limit) => {
+  const filteredResults = [];
+  const resultsCities = results.map(res => res.city);
+  for (
+    let i = 0;
+    i < results.length && (limit < 0 || filteredResults.length < limit);
+    i++
+  ) {
+    const city = results[i].city;
+    if (resultsCities.indexOf(city) >= i) {
+      //since preeceding city does not exist in sorted list, it means current occurence has worst aq
+      filteredResults.push(results[i]);
+    }
+  }
+  return filteredResults;
+};
 
 export const collectCities = async (
   countryCode,
@@ -17,38 +50,12 @@ export const collectCities = async (
     parameter: parameter || "pm25",
     limit: 10000
   };
-  //   const url = `https://api.openaq.org/v1/latest?country=${countryCode}&parameter=pm25&limit=1000`;
   const url = stringifyRequest(baseUrl + latestEndpoint, params);
   const latestResponse = await fetch(url);
   const latestJson = await latestResponse.json();
   const results = latestJson.results;
-  //format to given parameter
-  const resultsFormatted = results.map(res => ({
-    country: res.country,
-    city: res.city,
-    location: res.location,
-    measurement: res.measurements[0].value,
-    unit: res.measurements[0].unit
-  }));
-  //sort by measurement, descending order (most polluted cities first)
-  const sortedResults = resultsFormatted.sort(
-    (resA, resB) => resB.measurement - resA.measurement
-  );
-
-  //remove duplicates (if city has more locations), save occurence with worst air quality
-  const filteredResults = [];
-  const sortedResultsCities = sortedResults.map(res => res.city);
-  for (
-    let i = 0;
-    i < sortedResults.length && (limit < 0 || filteredResults.length < limit);
-    i++
-  ) {
-    const city = sortedResults[i].city;
-    if (sortedResultsCities.indexOf(city) >= i) {
-      //since preeceding city does not exist in sorted list, it means current occurence has worst aq
-      filteredResults.push(sortedResults[i]);
-    }
-  }
-
+  const resultsFormatted = formatResults(results);
+  const sortedResults = sortResults(resultsFormatted);
+  const filteredResults = limitAndFilterCityDuplicates(sortedResults, limit);
   return filteredResults;
 };
