c1fabcdd145c4d8f8c81a3bf49b066beec85dd95
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-17 09:07:18 +0200

    added footer openaq icon button

--- src/components/Headers/Footer.jsx
+++ src/components/Headers/Footer.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import styles from "./Headers.module.scss";
+import { ReactComponent as OpenAqLogo } from "../../assets/opanaq_icon.svg";
 
 const Footer = () => (
   <footer className={styles.Footer__wrapper}>
+    <button className={styles.Footer__button}>
+      <OpenAqLogo className={styles.Footer__icon} />
+    </button>
     <div>Credits</div>
   </footer>
 );
