f59ded165e37b9abdd39c9d3e4325966a053220e
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-17 20:42:42 +0200

    designed city data card header

--- src/components/Results/CityDataCard.jsx
+++ src/components/Results/CityDataCard.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { ReactComponent as ArrowLogo } from "../../assets/arrow_down_icon.svg";
 import styles from "./Results.module.scss";
 
-const CityDataSummary = () => <summary>nazwa miasta itp</summary>;
+const CityDataSummary = ({ city }) => (
+  <summary className={styles.CityDataCard__Summary}>
+    <ArrowLogo />
+    <div className={styles.CityDataCard__Header}>
+      <span className={styles.CityDataCard__Cityname}>{city.city}</span>
+      <div>
+        <span className={styles.CityDataCard__Parameter}>
+          {city.parameter}:&nbsp;
+        </span>
+        <span className={styles.CityDataCard__Value}>
+          {Math.round(city.value)}
+        </span>
+        <span className={styles.CityDataCard__Unit}>{city.unit}</span>
+      </div>
+    </div>
+  </summary>
+);
 
-const CityDataContent = () => <p>Informacje szczegółowe na dany temat.</p>;
+const CityDataContent = ({ city }) => (
+  <div className={styles.CityDataCard__Content}>
+    Informacje szczegółowe na dany temat.
+  </div>
+);
 
 const CityDataCard = ({ city }) => (
-  <details>
-    <CityDataSummary />
-    <CityDataContent />
+  <details className={styles.CityDataCard__Wrapper}>
+    <CityDataSummary city={city} />
+    <CityDataContent city={city} />
   </details>
 );
 export default CityDataCard;
 
-CityDataCard.propTypes = {
+const cityPropTypes = {
   city: PropTypes.shape({
     country: PropTypes.string.isRequired,
     city: PropTypes.string.isRequired,
@@ -22,5 +43,17 @@ CityDataCard.propTypes = {
     parameter: PropTypes.string.isRequired,
     value: PropTypes.number.isRequired,
     unit: PropTypes.string.isRequired
-  })
+  }).isRequired
+};
+
+CityDataSummary.propTypes = {
+  ...cityPropTypes
+};
+
+CityDataContent.propTypes = {
+  ...cityPropTypes
+};
+
+CityDataCard.propTypes = {
+  ...cityPropTypes
 };
