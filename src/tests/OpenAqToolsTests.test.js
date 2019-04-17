1e7cb3d86bb6521c17dafb8c7fc5e8a335848a62
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-17 17:27:34 +0200

    implemented tests for open aq data pipe, all pass

--- src/tests/OpenAqToolsTests.test.js
+++ src/tests/OpenAqToolsTests.test.js
@@ -94,4 +94,138 @@ describe("testing openaq data pipes", () => {
     ];
     expect(formatResults(rawResults)).toEqual(expectedResults);
   });
+
+  it("sorts results", () => {
+    const rawResults = [
+      {
+        location: "AM8 Gdańsk Wrzeszcz",
+        city: "Gdańsk",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.6072,
+        unit: "µg/m³"
+      },
+      {
+        location: "Białystok-Miejska",
+        city: "Białystok",
+        country: "PL",
+        parameter: "pm25",
+
+        value: 11,
+        unit: "µg/m³"
+      },
+      {
+        location: "Bielsko-Biała, ul.Partyzantów",
+        city: "Bielsko-Biała",
+        country: "PL",
+        parameter: "pm25",
+        value: 21.0775,
+        unit: "µg/m³"
+      }
+    ];
+
+    const expectedResults = [
+      {
+        location: "Bielsko-Biała, ul.Partyzantów",
+        city: "Bielsko-Biała",
+        country: "PL",
+        parameter: "pm25",
+        value: 21.0775,
+        unit: "µg/m³"
+      },
+      {
+        location: "Białystok-Miejska",
+        city: "Białystok",
+        country: "PL",
+        parameter: "pm25",
+
+        value: 11,
+        unit: "µg/m³"
+      },
+      {
+        location: "AM8 Gdańsk Wrzeszcz",
+        city: "Gdańsk",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.6072,
+        unit: "µg/m³"
+      }
+    ];
+    expect(sortResults(rawResults)).toEqual(expectedResults);
+  });
+
+  it("limits and filters duplicates", () => {
+    const rawResults = [
+      {
+        location: "Warszawa-Chrościckiego",
+        city: "Warszawa",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.83,
+        unit: "µg/m³"
+      },
+      {
+        location: "AM8 Gdańsk Wrzeszcz",
+        city: "Gdańsk",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.6072,
+        unit: "µg/m³"
+      },
+      {
+        location: "WIOŚ Olsztyn ul. Puszkina",
+        city: "Olsztyn",
+        country: "PL",
+        parameter: "pm25",
+        value: 9.26139,
+        unit: "µg/m³"
+      },
+      {
+        location: "WIOŚ Gołdap-Uzdrowisko",
+        city: "Gołdap",
+        country: "PL",
+        parameter: "pm25",
+        value: 8.04879,
+        unit: "µg/m³"
+      },
+      {
+        location: "Warszawa-Bajkowa",
+        city: "Warszawa",
+        country: "PL",
+        parameter: "pm25",
+        value: 7.86,
+        unit: "µg/m³"
+      }
+    ];
+    const limit = 3;
+    const expectedResults = [
+      {
+        location: "Warszawa-Chrościckiego",
+        city: "Warszawa",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.83,
+        unit: "µg/m³"
+      },
+      {
+        location: "AM8 Gdańsk Wrzeszcz",
+        city: "Gdańsk",
+        country: "PL",
+        parameter: "pm25",
+        value: 10.6072,
+        unit: "µg/m³"
+      },
+      {
+        location: "WIOŚ Olsztyn ul. Puszkina",
+        city: "Olsztyn",
+        country: "PL",
+        parameter: "pm25",
+        value: 9.26139,
+        unit: "µg/m³"
+      }
+    ];
+    expect(limitAndFilterCityDuplicates(rawResults, limit)).toEqual(
+      expectedResults
+    );
+  });
 });
