b23e6f13986bb784faab06a44777d18e41391331
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 11:33:03 +0200

    moved card content to separate file

---
+++ src/components/Results/CityDataCardContent.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./CityDataCardContent.module.scss";
+import { TextButton } from "../Common/Buttons/Buttons";
+import { cityKeywords } from "../../config/CityKeywords";
+
+const isDataCorrect = city => city.dataLoaded && city.data.articleSummary;
+const CityDataContentCorrect = ({ city }) => (
+  <>
+    <div className={styles.CityDataCard__ContentMain}>
+      {city.dataLoaded && <p>{city.data.articleSummary}</p>}
+    </div>
+    <div className={styles.CityDataCard__ContentFooter}>
+      <TextButton
+        text="more"
+        onClick={() => window.open(city.data.articleUrl, "_blank")}
+      />
+    </div>
+  </>
+);
+
+//if wiki text contains "refer" or does not contain any of city keywords
+//its marked as ambigious
+const isDataAmbigious = city =>
+  city.data.articleSummary.includes("refer") ||
+  !cityKeywords
+    .map(keyword => city.data.articleSummary.includes(keyword))
+    .some(x => x);
+const CityDataContentAmbigious = ({ city }) => (
+  <>
+    <div className={styles.CityDataCard__ContentMainAmbigious}>
+      <p>{city.data.articleSummary}</p>
+      <div>This seems a little ambigious, was it what you are looking for?</div>
+    </div>
+    <div className={styles.CityDataCard__ContentFooter}>
+      <TextButton
+        text="let me clear this up"
+        onClick={() => window.open(city.data.articleUrl, "_blank")}
+      />
+    </div>
+  </>
+);
+
+const CityDataContentFailed = ({ city }) => (
+  <>
+    <div className={styles.CityDataCard__ContentMainFailed}>
+      <p>Oops! It seems we could not find data you are looking for...</p>
+    </div>
+    <div className={styles.CityDataCard__ContentFooter}>
+      <TextButton
+        text="let me handle that"
+        onClick={() => window.open(city.data.articleUrl, "_blank")}
+      />
+    </div>
+  </>
+);
+
+const CityDataContent = ({ city }) => (
+  <div className={styles.CityDataCard__Content}>
+    {isDataCorrect(city) ? (
+      isDataAmbigious(city) ? (
+        <CityDataContentAmbigious city={city} />
+      ) : (
+        <CityDataContentCorrect city={city} />
+      )
+    ) : (
+      <CityDataContentFailed city={city} />
+    )}
+  </div>
+);
+export default CityDataContent;
--- src/config/CityKeywords.js
+++ src/config/CityKeywords.js
@@ -1,3 +1,5 @@
+//city and province synonyms, used for
+//recognition between correct and ambigious wiki response
 export const cityKeywords = [
   "capital",
   "city",
