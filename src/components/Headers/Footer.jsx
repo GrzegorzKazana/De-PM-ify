4d2c40e034fa9ab57fd6dec1371f021e9bed6814
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 12:12:59 +0200

    added button functionality to footer openaq icon

--- src/components/Headers/Footer.jsx
+++ src/components/Headers/Footer.jsx
@@ -4,10 +4,13 @@ import { ReactComponent as OpenAqLogo } from "../../assets/opanaq_icon.svg";
 
 const Footer = () => (
   <footer className={styles.Footer__wrapper}>
-    <button className={styles.Footer__button}>
+    <button
+      className={styles.Footer__button}
+      onClick={() => window.open("https://openaq.org/", "_blank")}
+    >
       <OpenAqLogo className={styles.Footer__icon} />
     </button>
-    <div>Credits</div>
+    <div>Air quality data by</div>
   </footer>
 );
 export default Footer;
