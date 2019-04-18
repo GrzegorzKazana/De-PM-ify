bd7407ca765a88b34118c55b8a22eb52820a38fd
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 22:23:04 +0200

    Updates

---
+++ precache-manifest.8c549ff24aaf770478bf4aef553486c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a87eca9ea0f39475f2fc7613a3af312f",
+    "url": "/de-pm-ify/static/media/search-icon.a87eca9e.svg"
+  },
+  {
+    "revision": "28a66f06c6cba23b18d828e8a67797e0",
+    "url": "/de-pm-ify/static/media/opanaq_icon.28a66f06.svg"
+  },
+  {
+    "revision": "c3d75740c5c58e55c4e6e9bac7e742ac",
+    "url": "/de-pm-ify/static/media/arrow_down_icon.c3d75740.svg"
+  },
+  {
+    "revision": "530cc4ee3844ebea9681",
+    "url": "/de-pm-ify/static/js/runtime~main.36af32fc.js"
+  },
+  {
+    "revision": "f0fbee46e6f49437beb7",
+    "url": "/de-pm-ify/static/js/main.cd6b7003.chunk.js"
+  },
+  {
+    "revision": "b19c3b777fe9dc41db99",
+    "url": "/de-pm-ify/static/js/2.e860af4a.chunk.js"
+  },
+  {
+    "revision": "f0fbee46e6f49437beb7",
+    "url": "/de-pm-ify/static/css/main.66269620.chunk.css"
+  },
+  {
+    "revision": "8f7f339b072dc012734339b608465c15",
+    "url": "/de-pm-ify/index.html"
+  }
+];
