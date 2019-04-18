ea86c6733ceacbc98a1c006f927eeb519ba3f4f1
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 13:30:17 +0200

    update hero design

--- src/components/Headers/Hero.jsx
+++ src/components/Headers/Hero.jsx
@@ -4,7 +4,6 @@ import styles from "./Headers.module.scss";
 const Hero = () => (
   <header className={styles.Hero__wrapper}>
     <h1>Lorem ipsum</h1>
-    <h3>subtitle</h3>
   </header>
 );
 export default Hero;
