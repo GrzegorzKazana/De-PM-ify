50dcc570f65384ac24f5cc6d0358e2c9d8a3225f
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 12:57:28 +0200

    added input local storage handling
    wrote custom useState hook

---
+++ src/utils/useStateLocalStorage.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const useStateLocalStorage = (keyName, initialState) => {
+  const [state, setState] = useState(initialState);
+
+  //run on mount
+  useEffect(() => {
+    const localData = localStorage.getItem(keyName);
+    setState(localData);
+  }, []);
+
+  //wrap setState
+  const setStateAndWriteLocal = newState => {
+    localStorage.setItem(keyName, newState);
+    setState(newState);
+  };
+
+  return [state, setStateAndWriteLocal];
+};
+export default useStateLocalStorage;
