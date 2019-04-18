c0dc3d26948d87483194106cfb0738de5c482c5c
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 11:33:03 +0200

    moved card content to separate file

--- src/components/Results/CityDataCard.jsx
+++ src/components/Results/CityDataCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Results.module.scss";
 import PropTypes from "prop-types";
 import { ReactComponent as ArrowLogo } from "../../assets/arrow_down_icon.svg";
-import { TextButton } from "../Common/Buttons/Buttons";
+import CityDataContent from "./CityDataCardContent";
 
 const CityDataSummary = ({ city }) => (
   <summary className={styles.CityDataCard__Summary}>
@@ -20,63 +20,6 @@ const CityDataSummary = ({ city }) => (
       </div>
     </div>
   </summary>
-);
-
-const CityDataContentCorrect = ({ city }) => (
-  <>
-    <div className={styles.CityDataCard__ContentMain}>
-      {city.dataLoaded && <p>{city.data.articleSummary}</p>}
-    </div>
-    <div className={styles.CityDataCard__ContentFooter}>
-      <TextButton
-        text="more"
-        onClick={() => window.open(city.data.articleUrl, "_blank")}
-      />
-    </div>
-  </>
-);
-
-const CityDataContentAmbigious = ({ city }) => (
-  <>
-    <div className={styles.CityDataCard__ContentMainAmbigious}>
-      <p>{city.data.articleSummary}</p>
-      <div>This seems a little ambigious, was it what you are looking for?</div>
-    </div>
-    <div className={styles.CityDataCard__ContentFooter}>
-      <TextButton
-        text="let me clear this up"
-        onClick={() => window.open(city.data.articleUrl, "_blank")}
-      />
-    </div>
-  </>
-);
-
-const CityDataContentFailed = ({ city }) => (
-  <>
-    <div className={styles.CityDataCard__ContentMainFailed}>
-      <p>Oops! It seems we could not find data you are looking for...</p>
-    </div>
-    <div className={styles.CityDataCard__ContentFooter}>
-      <TextButton
-        text="let me handle that"
-        onClick={() => window.open(city.data.articleUrl, "_blank")}
-      />
-    </div>
-  </>
-);
-
-const CityDataContent = ({ city }) => (
-  <div className={styles.CityDataCard__Content}>
-    {city.dataLoaded ? (
-      city.data.articleSummary.includes("refer") ? (
-        <CityDataContentAmbigious city={city} />
-      ) : (
-        <CityDataContentCorrect city={city} />
-      )
-    ) : (
-      <CityDataContentFailed city={city} />
-    )}
-  </div>
 );
 
 const CityDataCard = ({ city }) => (
