03936292f71f4bfb00c287f1e5d9050656fcaf72
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-18 20:09:06 +0200

    moved all url declaration to /config/urls

---
+++ src/config/Urls.js
@@ -0,0 +1,5 @@
+export const openAqLatestApi = "https://api.openaq.org/v1/latest";
+
+export const wikiOpenSearchApi = "https://en.wikipedia.org/w/api.php";
+
+export const wikiSearchUrl = "https://en.wikipedia.org/w/index.php?search=";
