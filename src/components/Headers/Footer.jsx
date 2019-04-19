dd61b858f364486902dba8fe7f7c778aed583620
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-19 09:38:39 +0200

    changed svg button redirecting outside to 'a' tag

--- src/components/Headers/Footer.jsx
+++ src/components/Headers/Footer.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import styles from "./Headers.module.scss";
 import { ReactComponent as OpenAqLogo } from "../../assets/opanaq_icon.svg";
 import { openAqPageUrl } from "../../config/Urls";
-import { openLink } from "../../utils/OpenLink";
 
 const Footer = () => (
   <footer className={styles.Footer__wrapper}>
-    <button
-      className={styles.Footer__button}
-      onClick={() => openLink(openAqPageUrl)}
+    <a
+      className={styles.Footer__hyperlink}
+      href={openAqPageUrl}
+      target="_blank"
     >
       <OpenAqLogo className={styles.Footer__icon} />
-    </button>
+    </a>
     <div>Air quality data by</div>
   </footer>
 );
