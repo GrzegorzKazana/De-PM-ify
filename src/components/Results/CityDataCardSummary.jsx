640fdfe70ef9c2c86e65446708e1c66548e49c0a
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-19 13:26:01 +0200

    restricted fetching data in subsequent card open/closes

--- src/components/Results/CityDataCardSummary.jsx
+++ src/components/Results/CityDataCardSummary.jsx
@@ -3,26 +3,32 @@ import styles from "./styles/Results.module.scss";
 import { ReactComponent as ArrowLogo } from "../../assets/arrow_down_icon.svg";
 import { cityPropTypes } from "./CityDataCard";
 
-const CityDataSummary = ({ city, requestCityData }) => (
-  <summary
-    className={styles.CityDataCard__Summary}
-    onClick={() => requestCityData(city)}
-  >
-    <ArrowLogo />
-    <div className={styles.CityDataCard__Header}>
-      <span className={styles.CityDataCard__Cityname}>{city.city}</span>
-      <div>
-        <span className={styles.CityDataCard__Parameter}>
-          {city.parameter}:&nbsp;
-        </span>
-        <span className={styles.CityDataCard__Value}>
-          {Math.round(city.value)}
-        </span>
-        <span className={styles.CityDataCard__Unit}>{city.unit}</span>
+const CityDataSummary = ({ city, requestCityData }) => {
+  const requestCityDataFetch = () => {
+    !city.dataLoaded && requestCityData(city);
+  };
+
+  return (
+    <summary
+      className={styles.CityDataCard__Summary}
+      onClick={requestCityDataFetch}
+    >
+      <ArrowLogo />
+      <div className={styles.CityDataCard__Header}>
+        <span className={styles.CityDataCard__Cityname}>{city.city}</span>
+        <div>
+          <span className={styles.CityDataCard__Parameter}>
+            {city.parameter}:&nbsp;
+          </span>
+          <span className={styles.CityDataCard__Value}>
+            {Math.round(city.value)}
+          </span>
+          <span className={styles.CityDataCard__Unit}>{city.unit}</span>
+        </div>
       </div>
-    </div>
-  </summary>
-);
+    </summary>
+  );
+};
 export default CityDataSummary;
 
 CityDataSummary.propTypes = {
