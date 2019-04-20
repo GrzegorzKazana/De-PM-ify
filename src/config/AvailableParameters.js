99cb3690a69e8f4740bd0c7e92367ca810f28b44
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-20 21:39:05 +0200

    defined norms for parameters

--- src/config/AvailableParameters.js
+++ src/config/AvailableParameters.js
@@ -1,7 +1,9 @@
+//health norms according to
+//http://ec.europa.eu/environment/air/quality/standards.htm
 export default [
-  { value: "pm25", label: "PM2.5" },
-  { value: "pm10", label: "PM10" },
-  { value: "no2", label: "NO2" },
-  { value: "o3", label: "O3" },
-  { value: "co", label: "CO" }
+  { value: "pm25", label: "PM2.5", norm: 25 },
+  { value: "pm10", label: "PM10", norm: 50 },
+  { value: "no2", label: "NO2", norm: 200 },
+  { value: "o3", label: "O3", norm: 120 },
+  { value: "co", label: "CO", norm: 10000 }
 ];
