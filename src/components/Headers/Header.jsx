bf521f005f76145a5ff57a1387f5c0076096bfa5
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-20 21:27:02 +0200

    added header subtitl

--- src/components/Headers/Header.jsx
+++ src/components/Headers/Header.jsx
@@ -3,7 +3,8 @@ import styles from "./Headers.module.scss";
 
 const Header = () => (
   <header className={styles.Hero__wrapper}>
-    <h1>De-PM-ify</h1>
+    <h1>de-PM-ify</h1>
+    <h2>track most polluted places in Europe</h2>
   </header>
 );
 export default Header;
