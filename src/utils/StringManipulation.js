a2366bcbe27b28dcc4c20395d7ed1d95437df7a6
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-20 17:27:42 +0200

    fixed addtional rule to title formatting, improves wiki search

--- src/utils/StringManipulation.js
+++ src/utils/StringManipulation.js
@@ -7,7 +7,7 @@ export const fixCapitalization = string =>
     .split(" ")
     .map(s => s.charAt(0).toUpperCase() + s.substring(1))
     .join(" ")
-    .replace(/-([a-z])/, (match, g1) => "-" + g1.toUpperCase());
+    .replace(/-([a-z])/, (_, g1) => "-" + g1.toUpperCase());
 
 export const takeUntilChar = (string, char) => string.split(char)[0];
 
