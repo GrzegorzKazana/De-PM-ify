e4db489605b97254dd463c525246fa5757b43740
Grzegorz Kazana (grzekaz068) <grzekaz068@student.polsl.pl>
2019-04-20 18:50:01 +0200

    removed unused file

--- src/config/RedundantCityPrefixes.js
+++
@@ -1,9 +0,0 @@
-//following city/province prefixes carry no value
-//and provent wiki api from finding correct data
-export const redundantCityPrefixes = [
-  "Air ",
-  "Atmo ",
-  "Ccaa ",
-  "Com. ",
-  "Cast. "
-];
